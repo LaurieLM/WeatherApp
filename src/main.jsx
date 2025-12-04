0579f9c07e7b1ee00870303cdd21fa4e324c2d98
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-12-04 12:48:41 +0100

    Creating Layout with an Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './style.css'
+import Layout from './pages/Layout.jsx';
+
 
 const router = createBrowserRouter([
   {
