40b0b046eb0c6bc6df82d1047eb0c623e764e087
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-12-05 14:11:48 +0100

    add style title

--- src/components/FirstTitle.jsx
+++ src/components/FirstTitle.jsx
@@ -1,5 +1,5 @@
 export default function FirstTitle() {
     return (
-    <h1>How's the sky looking today ?</h1>
+    <h1 className="text-center text-[14vw] font-bold px-[6vw] py-[4vh] leading-tight">How's the sky looking today ?</h1>
     )
 }
