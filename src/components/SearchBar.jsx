3cfc579d50f679ff7f0412607e2223cfe6c05481
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-12-05 15:15:40 +0100

    style

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,13 @@ export default function SearchBar() {
     return (
         <div className="relative flex flex-col items-center">
             <img src="./src/assets/images/icon-search.svg" alt="Search Icon" className="absolute left-[42px] top-[12px] w-[6vw]"/>
-            <input type="search" name="location" placeholder="Search for a place..." className="w-[90vw] h-[7.5vh] bg-[hsl(243_23%_24%)] text-center rounded-lg placeholder:text-[5.5vw] placeholder:text-[hsl(240_6%_70%)]" />
+            <input
+                type="search"
+                maxLength={20}
+                name="location"
+                placeholder="Search for a place..."
+                className="w-[90vw] h-[7.5vh] bg-[hsl(243_23%_24%)] text-center rounded-lg placeholder:text-[5.5vw] placeholder:text-[hsl(240_6%_70%)] outline-none" />
+                
             <button type="submit" className="bg-[hsl(233_67%_56%)] w-[90vw] h-[7.5vh] rounded-lg text-center text-[5vw] mt-[2vh]">Search</button>
         </div>
     )
